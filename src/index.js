8075b4dafd41f04d9871e207ef8209ecaef807d2
Nikhil Mittal <nmittal1_be22@thapar.edu>
2024-04-24 21:43:14 +0530

    util code , api response and error handling

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ import { connectDB } from "./db/index.js";
 dotenv.config({path: './env'});
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 ,()=>{
+        console.log(`App is running on port ${process.env.PORT || 8000}`);
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed",err);
+})
 
 
 
