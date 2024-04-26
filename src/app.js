3ddf6b8be7cb41f14238074bc2eba732f0775214
Nikhil Mittal <nmittal1_be22@thapar.edu>
2024-04-27 01:48:44 +0530

    register user  router and controller added

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
+
+
 const app = express();
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
@@ -12,4 +14,15 @@ app.use(express.json({limit: "16kb"}));
 app.use(express.urlencoded({extended: true,limit: "16kb"}));
 app.use(cookieParser());
 app.use(express.static("public"));
+
+
+//routes
+import userRouter from "./routes/user.routes.js";
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter);
+
+//https:localhost:4000/api/v1/users/
+
 export {app}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,10 @@
 const asyncHandler = (requestHandler) => {
-    (req,res,next) => {
+    return (req,res,next) => {
         Promise.resolve(requestHandler(req,res,next)).
         catch((err) => next(err))
 }}
 
-export {asyncHandler};
+export default asyncHandler;
 
 
 // const asyncHandler1 = (fn)=> async (req,res,next)=>{
